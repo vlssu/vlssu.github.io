ffee1432ead56c503fd98ba8a5a838311f90aa2c
飒爽师叔 <43847794+vlssu@users.noreply.github.com>
2022-09-24 23:15:31 +0800

    Update config.ts

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -126,20 +126,24 @@ export default defineUserConfig({
     //   ],
     // },
     // valineConfig 配置与 1.x 一致
-    valineConfig: {
-      appId: 'RvrM45f9SVD3qeRUSz1Q7rkx-9Nh9j0Va',
-      appKey: 'gpUrlXJ3ISpcmTOqXBzd01gY',
-      placeholder: "粘贴图片到此处可以自动上传，填写邮箱可以邮件收到回复哦(●'◡'●)",
-      requiredFields: ['nick','mail'], // 必要值
-      lang:'zh-CN', // 语言
-      notify: true, // 邮件提醒
-      verify: false, // 验证码服务
-      visitor: true, // 阅读量统计
-      recordIP: true, // 记录评论者IP
-      enableQQ: true, // 是否开启强制拉取 qq 头像
-      // hideComments: true, // 隐藏评论
-      background: '/comment.webp'  // 背景图
+    commentConfig: {
+      type: 'valine',
+      options: {
+        appId: 'RvrM45f9SVD3qeRUSz1Q7rkx-9Nh9j0Va',
+        appKey: 'gpUrlXJ3ISpcmTOqXBzd01gY',
+        placeholder: "粘贴图片到此处可以自动上传，填写邮箱可以邮件收到回复哦(●'◡'●)",
+        requiredFields: ['nick','mail'], // 必要值
+        lang:'zh-CN', // 语言
+        notify: true, // 邮件提醒
+        verify: false, // 验证码服务
+        visitor: true, // 阅读量统计
+        recordIP: true, // 记录评论者IP
+        enableQQ: true, // 是否开启强制拉取 qq 头像
+        // hideComments: true, // 隐藏评论
+        background: '/comment.webp'  // 背景图
+      },
     },
   }),
+  lang: 'zh-CN',
   // debug: true,
 })
