f69ff00bf11f116bf8b797a7fc7d74988c411cb8
飒爽师叔 <43847794+vlssu@users.noreply.github.com>
2025-05-11 15:31:06 +0800

    切换为国内可以访问的评论地址

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -142,7 +142,7 @@ export default hopeTheme({
     // 警告: 这是一个仅供演示的测试服务，在生产环境中请自行部署并使用自己的服务！
     comment: {
       provider: "Waline",
-      serverURL: "https://waline-vlssus-projects.vercel.app",
+      serverURL: "https://waline-comment.vlssu.com",
     },
 
     components: {
